12fb83967f20f0f0c2d682c4be667f70f36c00da
Shivam Patel <shivampatel0048@gmail.com>
2024-08-21 16:32:29 +0530

    UI: logo and icon added

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set up Axios instance
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/",
   headers: {
     "Content-Type": "application/json",
   },
